c8aedca2d7f18e370d644d8ce56e9465718d6287
Tim Schottler <tim.schottler@cliniciannexus.com>
2024-10-12 16:54:43 -0500

    fix height

--- client/src/inventory-hud.js
+++ client/src/inventory-hud.js
@@ -5,7 +5,7 @@ import { InventoryWidth, InventoryHeight } from '../../shared/constants.js'
 class InventoryHud extends Container {
   constructor() {
     super()
-    const bg = new Graphics().rect(0, 0, InventoryWidth, InventoryWidth).fill(0xffffff)
+    const bg = new Graphics().rect(0, 0, InventoryWidth, InventoryHeight).fill(0xffffff)
     bg.alpha = 0.5
     this.addChild(bg)
 
--- shared/constants.js
+++ shared/constants.js
@@ -1,7 +1,7 @@
 const ArtScale = 1.5
 const BlockSize = 32
 const MinimapScale = 0.2
-const InventoryWidth = 600
-const InventoryHeight = 800
+const InventoryWidth = 500
+const InventoryHeight = 600
 
 export { ArtScale, BlockSize, MinimapScale, InventoryWidth, InventoryHeight }
