9102dd4b3352f381da208786ca91617085fac985
Tim Schottler <tim.schottler@cliniciannexus.com>
2024-10-12 16:55:25 -0500

    change size per sam

--- shared/constants.js
+++ shared/constants.js
@@ -1,7 +1,7 @@
 const ArtScale = 1.5
 const BlockSize = 32
 const MinimapScale = 0.2
-const InventoryWidth = 500
+const InventoryWidth = 400
 const InventoryHeight = 600
 
 export { ArtScale, BlockSize, MinimapScale, InventoryWidth, InventoryHeight }
