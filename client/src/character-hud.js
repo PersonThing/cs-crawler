8d5ce64b5e1f875acdf611ffc72eb2c32bc5540f
Tim Schottler <tim.schottler@cliniciannexus.com>
2025-10-03 21:08:18 -0500

    remove unused code

--- client/src/character-hud.js
+++ client/src/character-hud.js
@@ -1,17 +1,9 @@
-import { Graphics, Container, Sprite, Text } from 'pixi.js'
-import { Textures } from './textures.js'
+import { Graphics, Container, Text } from 'pixi.js'
 
 const STAT_SIZE = 18
 const STAT_MARGIN = 4
 const CHARACTER_SHEET_WIDTH = 200
 const CHARACTER_SHEET_HEIGHT = 300
-
-const getStatCoordinates = (x, y) => {
-  return {
-    x: x * (STAT_SIZE + STAT_MARGIN * 2) + STAT_SIZE / 2,
-    y: y * (STAT_SIZE + STAT_MARGIN * 2) + 20,
-  }
-}
 
 class InventoryHud extends Container {
   constructor(app, player) {
@@ -98,76 +90,6 @@ class InventoryHud extends Container {
         })
         this.statContainer.addChild(statText)
       })
-  }
-
-  setStats(content) {
-    this.content = content
-
-    // re-render background so equipped slot sprites go away
-    this.renderBackground()
-
-    if (this.itemContainer != null) {
-      this.removeChild(this.itemContainer)
-      this.itemContainer.destroy()
-    }
-
-    this.itemContainer = new Container()
-    this.addChild(this.itemContainer)
-
-    // bags
-    for (let index = 0; index < BAG_SLOTS; index++) {
-      const item = content.bags[index]
-      if (item != null) {
-        const coords = this.getBagSlotCoordinates(index)
-        const itemSprite = this.drawItem(item, coords)
-        itemSprite.on('mousedown', () => {
-          console.log('filled bag slot click', index)
-          this.playerInventory.clickBagSlot(index)
-        })
-      }
-    }
-
-    // equipped
-    Object.keys(content.equipped).forEach(slotName => {
-      const item = content.equipped[slotName]
-      if (item == null) {
-        return
-      }
-
-      const coords = EquippedSlotCoordinates[slotName]
-      const itemSprite = this.drawItem(item, coords)
-      itemSprite.on('mousedown', () => {
-        console.log('filled equipped slot click', slotName)
-        this.playerInventory.clickEquippedSlot(slotName)
-      })
-
-      // if 2h weapon, render a greyed out version of sprite
-      if (slotName === InventorySlot.MainHand.name && item.itemType.bothHands) {
-        const coords = EquippedSlotCoordinates[InventorySlot.OffHand.name]
-        this.drawItem(item, coords, true)
-        // itemSprite.on('mousedown', (e) => {
-        //   console.log('filled 2h slot offhand click', slotName)
-        //   this.playerInventory.clickEquippedSlot(slotName)
-        // })
-      }
-    })
-
-    // cursor
-    if (content.cursor != null) {
-      const color = ItemQualityColors[content.cursor.itemQuality]
-      this.cursorBg = this.drawItemBg(color, { x: 0, y: 0 })
-      this.cursorBg.eventMode = 'none'
-      this.cursorItem = this.drawItem(content.cursor, { x: 0, y: 0 })
-      this.cursorItem.eventMode = 'none'
-      this.setCursorItemPosition()
-      // TODO move these with mouse
-    }
-  }
-
-  drawItem(item, { x, y }, isDisabledOffHand) {
-    const inventoryItem = new InventoryItem(item, { x, y }, isDisabledOffHand)
-    this.itemContainer.addChild(inventoryItem)
-    return inventoryItem
   }
 }
 
