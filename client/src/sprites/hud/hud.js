d77dfb1120c0750345ccc0c94498f4123afe16c5
Tim Schottler <tim.schottler@cliniciannexus.com>
2025-10-07 09:30:50 -0500

    rearrange hud so inventory is on top of everything, minimap below everything

--- client/src/sprites/hud/hud.js
+++ client/src/sprites/hud/hud.js
@@ -11,10 +11,9 @@ import PlayerControls from '../../player-controls.js'
 class Hud extends Container {
   constructor(app, world, levelConfig) {
     super()
-
-    this.inventory = new InventoryHud()
-    this.addChild(this.inventory)
-    this.inventory.visible = DEBUG.get()
+    
+    this.minimap = new Minimap(levelConfig, false)
+    this.addChild(this.minimap)
 
     this.character = new CharacterHud()
     this.addChild(this.character)
@@ -26,29 +25,30 @@ class Hud extends Container {
 
     this.players = new PlayersHud()
     this.addChild(this.players)
-    
-    this.minimap = new Minimap(levelConfig, false)
-    this.addChild(this.minimap)
+
+    this.inventory = new InventoryHud()
+    this.addChild(this.inventory)
+    this.inventory.visible = DEBUG.get()
     
     screenSizeStore.subscribe(({ width, height }) => {
       // minimap aligned to top right
       this.minimap.x = width - this.minimap.width / 2 - 10
       this.minimap.y = 0
 
-      // inventory aligned to bottom right of screen
-      this.inventory.x = width - this.inventory.width
-      this.inventory.y = height - this.inventory.height
+      // players list aligned to top right of screen, under minimap
+      this.players.x = width - HUD_PLAYERS_WIDTH
+      this.players.y = MINIMAP_HEIGHT + 10
 
       // character sheet aligned to bottom left of screen
       this.character.y = height - this.character.height
 
+      // inventory aligned to bottom right of screen
+      this.inventory.x = width - this.inventory.width
+      this.inventory.y = height - this.inventory.height
+
       // help sheet aligned to top left of screen
       this.help.x = 10
       this.help.y = 10
-
-      // players list aligned to top right of screen, under minimap
-      this.players.x = width - HUD_PLAYERS_WIDTH
-      this.players.y = MINIMAP_HEIGHT + 10
     })
 
     this.playerControls = new PlayerControls(app, world, this.minimap, this)
